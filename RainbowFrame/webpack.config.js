b731fd0d87632e56ac1a43f3aebf7c619b56be0c
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-07-25 11:21:22 +0300

    .

--- RainbowFrame/webpack.config.js
+++ RainbowFrame/webpack.config.js
@@ -1,34 +1,13 @@
-const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const MiniCssExtractPlugin= require("mini-css-extract-plugin");;
-
-const extractCSS = new MiniCssExtractPlugin({
-    filename: "bundle.css"
-});
-
-module.exports = { 
-    entry: "./index.js", // основной файл приложения
-    output:{ 
-        path: path.resolve(__dirname, 'build'), // путь к каталогу выходных файлов
-        filename: "bundle.js"  // название создаваемого файла 
-    }, 
-    devtool:'source-map',
-    module:{ 
-        rules:[
-            { 
-                test: /\.jsx?$/, // какие файлы обрабатывать
-                exclude: /node_modules/, // какие файлы пропускать
-                use: { loader: "babel-loader" }
-            },
-            {
-                test: /\.css$/,
-                use: MiniCssExtractPlugin.loader({
-                    use: ["css-loader"]
-                })
-            }            
-        ] 
-    },
-    plugins: [
-        extractCSS
-    ]
-}
+module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
