aaefd77b1e201f7493e1bc10734b30dbda182010
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-17 16:44:33 +0300

    .

--- Mobile/App.js
+++ Mobile/App.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 
 import MobileCompany from './components/MobileCompany/MobileCompany';
 
-let companyName = 'A1';
 let clientsArr = [
   { id: 101, FIO: { fam: "Иванов", im: "Иван", otch: "Иванович" }, balance: 200 },
   { id: 105, FIO: { fam: "Сидоров", im: "Сидор", otch: "Сидорович" }, balance: 250 },
@@ -15,7 +14,6 @@ let clientsArr = [
 
 ReactDOM.render(
   <MobileCompany
-    name={companyName}
     clients={clientsArr}
   />
   , document.getElementById('container')
