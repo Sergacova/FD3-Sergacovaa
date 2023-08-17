8d31d1223ba689b81944307f1d0b84e7675d38cf
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-17 12:11:37 +0300

    .

--- ishop3/ishop3/src/components/index.js
+++ ishop3/ishop3/src/components/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './src/App.jsx';
+import App from './src/data/App.jsx';
 
 const productsArr = require('./src/data/products.json');
 const shopName = 'ISHOP3';
