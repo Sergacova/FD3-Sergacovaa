d67515b4dbb41c3fec937b6c2594bcae237df476
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-17 12:32:41 +0300

    .

--- ishop3/webpack.config.js
+++ ishop3/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const extractCSS = new ExtractTextPlugin({
     filename: "bundle.css"
