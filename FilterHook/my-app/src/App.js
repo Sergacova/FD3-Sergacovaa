20cca81a6ce7db29345102bbb8ffd9ecfab00291
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-21 12:21:12 +0300

    .

---
+++ FilterHook/my-app/src/App.js
@@ -0,0 +1,18 @@
+import filter from './components/Filter';
+import controls from './components/Controls';
+import list from './components/List';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <filter>
+      <controls>
+      <list>
+
+      </list>
+      </controls>
+      </filter>
+    </div>
+  );}
+
+export default App;
