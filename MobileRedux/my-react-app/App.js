d814241d906ec080e30468c9a8ca1da291fd614f
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-22 09:50:26 +0300

    .

--- MobileRedux/my-react-app/App.js
+++ MobileRedux/my-react-app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MobileCompany from './components/MobileCompany';
+import MobileCompany from './src/components/MobileCompany';
 
 let clientsArr=[ 
   {id:101, fam:"Иванов", im:"Иван", otch:"Иванович", balance:200}, 
