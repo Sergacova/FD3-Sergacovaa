adcaf08857d031798ccef0708997454c31d3cd24
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-23 16:07:33 +0300

    .

---
+++ RainbowFrameHOCNEW/App.js
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+import { withRainbowFrame } from './components/withRainbowFrame';
+import DoubleButton from './components/DoubleButton';
+
+const App = () => {
+  let colors = ['red', 'orange', 'yellow', 'green', 'grey', 'blue', 'purple'];
+  let FramedDoubleButton = withRainbowFrame(colors)(DoubleButton);
+  return (
+    <FramedDoubleButton
+      caption1="я из лесу"
+      caption2="мороз"
+      cbPressed={(num) => alert(num)}
+    >
+      вышел, был сильный
+    </FramedDoubleButton>
+  );
+};
+
+export default App;
