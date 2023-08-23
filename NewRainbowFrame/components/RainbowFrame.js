65bc97f4521deadd2b55347b94430406f9daa6a2
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-23 15:37:58 +0300

    .

---
+++ NewRainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Block from './VotesBlock';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+  
+
+render() {
+  /*
+    var n=this.props.colors.length
+    var divArr=[]     
+    var elem=this.props.children
+    for (var i=1;i<=n;i++){                 
+        elem=<div style={{border:"solid 4px "+this.props.colors[i],padding:"10px",  width:40*i, marginLeft:"6px"}}  key={i} children={elem}></div>           
+
+    }
+    divArr.push(elem)
+
+
+    return(  
+    divArr     
+        )    
+      }
+      */
+if (this.props.colors.length==0){
+  return this.props.children
+}
+else {
+  return (<div style={{border:"solid 4px "+this.props.colors[0],padding:"10px",  width:"100%", marginLeft:"6px"}}>
+    <RainbowFrame colors={this.props.colors.slice(1)}>{this.props.children}</RainbowFrame>
+  </div>)
+}
+
+    }    
+}
+export default RainbowFrame;
---
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+
+function withRainbowFrame (colors){
+  
+
+
+return function(Component) {
+  return function (props) {
+  var elem=<Component {...props}/>
+  colors.forEach ((i,v)=>{                 
+      elem=<div style={{border:"solid 4px "+i, padding:"10px",  width:270+40*v, marginLeft:"6px"}}  key={v} children={elem}></div> 
+         
+  })
+ 
+  return  elem
+  
+  }
+};
+}
+
+export  {withRainbowFrame};
