26efc2d578055a59009b6a482172a5240338c361
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-09-20 12:54:51 +0300

    .

--- movies/src/layout/Header.jsx
+++ movies/src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header () {
     return <nav className ="purple accent-3">
         <div className="nav-wrapper">
-            <a href= "#" classNameName= "brand-logo">React Movies</a>
+            <a href= "#" className= "brand-logo">React Movies</a>
             <ul id= "nav-mobile" className="right hide-on-med-and-dowrn">
             <li><a href="!#">Repo</a></li>
     
--- movies/src/layout/Main.jsx
+++ movies/src/layout/Main.jsx
@@ -1,8 +1,48 @@
-function Main (){
-    return <main className="container content">
-Hello from Movies
+import React from "react"
+import {Movies} from "../components/Movies"
+import { Preloader } from "../components/Preloader"
+import { Search } from "../components/Search"
 
-    </main>
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount (){
+        fetch("http://www.omdbapi.com/?s=matrix&apikey=76945206")
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+    componentDidMount (){
+        fetch("http://www.omdbapi.com/?apikey=76945206&s=game")
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+    searchMovies = (str) => {
+        fetch(`http://www.omdbapi.com/?s=matrix&apikey=${str}`)
+        .then(data => this.setState({movies: data.Search}))
+    }
+    searchMovies = (str) => {
+        fetch(`http://www.omdbapi.com/?apikey=76945206&s=${str}`)
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+
+    render () {
+        const {movies} = this.state;
+
+        
+    return <main className="container content">
+        <Search searchMovies={this.searchMovies} />
+        {
+            movies.length ? 
+            ( <Movies movies={this.state.movies}/>
+            ): <Preloader />
+        }
+    
+        <Movies movies={this.state.movies}/></main>
+
+}
 }
 
 export {Main}
