6fd47d992e7209c977559b1e5929308718eb5a4d
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-09-20 09:20:26 +0300

    .

---
+++ movies/src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+function Header () {
+    return <nav>
+        <div className="nav-wrapper">
+            <a href= "#" classNameName= "brand-logo">React Movies</a>
+            <ul id= "nav-mobile" className="right hide-on-med-and-dowrn">
+            <li><a href="sass.html">Sass</a></li>
+    
+                </ul>        
+                </div>
+    </nav>
+}
