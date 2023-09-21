f0e620954f5621aa663d6e50e2869c4689b72409
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-09-21 09:02:27 +0300

    .

---
+++ movies/src/layout/a.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import {Movies} from "../components/Movies"
+import { Preloader } from "../components/Preloader"
+import { Search } from "../components/Search"
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount (){
+        fetch("http://www.omdbapi.com/?s=matrix&apikey=76945206")
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+    componentDidMount (){
+        fetch("http://www.omdbapi.com/?apikey=76945206&s=game")
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+    c
+
+    render () {
+        const {movies} = this.state;
+
+        
+        return <main className="container content">
+        <Search searchMovies={this.searchMovies} />
+        {
+            movies.length ? 
+            ( <Movies movies={this.state.movies}/>
+            </main>
+        
+            ):  }
+            <Preloader />
+        
+    
+        <Movies movies={this.state.movies}/></main>
+
+}
+}
+
+export {Main}
